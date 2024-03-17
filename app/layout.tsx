d9aa531c05f96cf64f580c8e1db1f66a22db011a
awocato <minikhanov.talgat@gmail.com>
2024-03-17 22:33:34 +0500

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 
+
 export const metadata: Metadata = {
   title: "awoshop",
   description: "Shop Websiste practice using Next.js",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://i.postimg.cc/m2w5CWPy/awoshop.png",
+        alt: "awoshop",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
